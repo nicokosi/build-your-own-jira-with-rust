25f8e77b4466edfe3eaa7ec786d1fdb219ad2add
Nicolas Kosinski <nicokosi@yahoo.com>
2020-03-29 13:53:35 +0200

    (01) ticket - (01) ticket

--- jira-wip/src/path_to_enlightenment.rs
+++ jira-wip/src/path_to_enlightenment.rs
@@ -1,3 +1,4 @@
 // This file is handled by `koans-framework` so you will not need to modify it manually.
 include!("koans/00_greetings/00_greetings.rs");
 include!("koans/01_ticket/01_ticket.rs");
+include!("koans/01_ticket/02_status.rs");
